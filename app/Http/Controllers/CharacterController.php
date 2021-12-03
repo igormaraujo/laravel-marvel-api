bf22b636ddab69b419fd227fab95dd9061399a6b
igormaraujo <igoraraujo@ufpa.br>
2021-12-03 17:23:49 -0300

    feat: get character details

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -6,6 +6,7 @@ use App\Models\Character;
 use Illuminate\Http\Request;
 use App\Services\Marvel;
 use App\Http\Requests\CharacterRequest;
+use App\Models\Comic;
 use Illuminate\Support\Facades\Cache;
 
 class CharacterController extends Controller
@@ -46,22 +47,22 @@ class CharacterController extends Controller
     switch ($type . '-' . $encoding) {
       case 'json-gzip':
         $result = gzencode($data->body(), 3);
-        Cache::put($key, $result, now()->addMinutes(5));
+        Cache::put($key, $result, now()->addHours(5));
         return response($result, 200)->header('Content-type', 'application/json')->header('Content-Encoding', 'gzip');
       case 'json-none':
         $result = $data->body();
-        Cache::put($key, $result, now()->addMinutes(5));
+        Cache::put($key, $result, now()->addHours(5));
         return response($result, 200)->header('Content-type', 'application/json');
       case 'html-gzip':
         $result = gzencode(
           view('characters.index', ['characters' => $data->json()["data"]])->render(),
           3
         );
-        Cache::put($key, $result, now()->addMinutes(5));
+        Cache::put($key, $result, now()->addHours(5));
         return response($result, 200)->header('Content-Encoding', 'gzip');
       case 'html-none':
         $result = view('characters.index', ['characters' => $data->json()["data"]])->render();
-        Cache::put($key, $result, now()->addMinutes(5));
+        Cache::put($key, $result, now()->addHours(5));
         return response($result, 200);
     }
   }
@@ -72,9 +73,70 @@ class CharacterController extends Controller
    * @param  \App\Models\Character  $character
    * @return \Illuminate\Http\Response
    */
-  public function show(Character $character)
+  public function show(Request $request, $character_id)
   {
-    //
+    $encoding = str_contains($request->headers->get("accept-encoding"), "gzip") ? "gzip" : "none";
+    $key = $encoding . '-characters-' . $character_id;
+    $cache = Cache::get($key);
+    if ($cache) {
+      switch ($encoding) {
+        case 'gzip':
+          return response($cache, 200)->header('Content-Encoding', 'gzip');
+        case 'none':
+          return response($cache, 200);
+      }
+    }
+    $character = Character::find($character_id);
+    if (!$character) {
+      $data = Marvel::get('characters/' . $character_id);
+      if ($data->failed()) {
+        return $data->body();
+      }
+      $response = $data->json();
+      $character = Character::create([
+        'id' => $response["data"]["results"][0]["id"],
+        'name' => $response["data"]["results"][0]["name"],
+        'description' => $response["data"]["results"][0]["description"] ?? '',
+        'resourceURI' => $response["data"]["results"][0]["urls"][0]["url"],
+        'thumbnail' => $response["data"]["results"][0]["thumbnail"]["path"] . "/portrait_uncanny." . $response["data"]["results"][0]["thumbnail"]["extension"],
+        'etag' => $response["etag"],
+      ]);
+
+      $offset = 0;
+      $comics = [];
+      do {
+        $response = Marvel::get('characters/' . $character_id . '/comics', ['limit' => 100, 'offset' => $offset])->json();
+        $total = $response["data"]["total"];
+        foreach ($response["data"]["results"] as $comic) {
+          $comics[] = [
+            'id' => $comic["id"],
+            'title' => $comic["title"],
+            'issn' => $comic["issn"],
+            'description' => $comic["description"] ?? '',
+            'resourceURI' => $comic["urls"][0]["url"],
+            'thumbnail' => $comic["thumbnail"]["path"] . "/portrait_uncanny." . $comic["thumbnail"]["extension"],
+            'etag' => '',
+          ];
+        }
+        $offset += 100;
+      } while($offset < $total);
+      Comic::upsert($comics, ['id'], ['title', 'issn', 'description', 'resourceURI', 'thumbnail']);
+      $character->comics()->sync(array_map(fn ($comic) => $comic['id'], $comics));
+    }
+    // ddd($character->comics()->get());
+    switch ($encoding) {
+      case 'gzip':
+        $result = gzencode(
+          view('characters.show', ['character' => $character, 'comics' => $character->comics()->get()])->render(),
+          3
+        );
+        Cache::put($key, $result, now()->addHours(5));
+        return response($result, 200)->header('Content-Encoding', 'gzip');
+      case 'none':
+        $result = view('characters.show', ['character' => $character, 'comics' => $character->comics()->get()])->render();
+        Cache::put($key, $result, now()->addHours(5));
+        return response($result, 200);
+    }
   }
 
   /**
