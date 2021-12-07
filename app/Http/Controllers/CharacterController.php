5115f3378fd48c356c0646d0d96f5f08f6ec98f1
igormaraujo <igoraraujo@ufpa.br>
2021-12-07 15:29:59 -0300

    feat: list characters

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -30,7 +30,7 @@ class CharacterController extends Controller
   public function index(CharacterRequest $request)
   {
     $default = [
-      'orderBy' => '-modified',
+      'orderBy' => 'name',
       'offset' => 0,
       'limit' => 10,
     ];
@@ -52,9 +52,39 @@ class CharacterController extends Controller
       }
     }
     $data = Marvel::get('characters', $params);
-    if ($data->failed()) {
-      return $data->body();
-    }
+    if ($data->failed() || gettype($data->json()) != "array" || $data->json() === []) {
+      $query = Character::orderBy($params["orderBy"]);
+      if(array_key_exists('nameStartsWith', $params)) {
+        $query->where('name', 'like', $params['nameStartsWith'] . '%');
+      }
+      $total = $query->count();
+      $query->offset($params['offset']);
+      $query->limit($params['limit']);
+      $data = [
+        'results' => $query->get()->toArray(),
+        'count' => $query->count(),
+        'offset' => $params['offset'],
+        'total' => $total,
+      ];
+      $current = (int) ($params["offset"] + $params["limit"]) / $params["limit"];
+      $pagination = [
+        'first' => 1,
+        'previus' => $current - 1,
+        'current' => $current,
+        'next' => $current + 1,
+        'last' => (int) ceil($data["total"] / $params["limit"]),
+      ];
+      $result = view('characters.index', ['cache'=> true, 'characters' => $data, 'pagination' => $pagination, 'params' => $request->validated()])->render();
+      return response($result, 503);
+    // }
+    $current = (int) ($params["offset"] + $params["limit"]) / $params["limit"];
+    $pagination = [
+      'first' => 1,
+      'previus' => $current - 1,
+      'current' => $current,
+      'next' => $current + 1,
+      'last' => (int) ceil($data->json()["data"]["total"] / $params["limit"]),
+    ];
     switch ($type . '-' . $encoding) {
       case 'json-gzip':
         $result = gzencode($data->body(), 3);
@@ -66,13 +96,13 @@ class CharacterController extends Controller
         return response($result, 200)->header('Content-type', 'application/json');
       case 'html-gzip':
         $result = gzencode(
-          view('characters.index', ['characters' => $data->json()["data"]])->render(),
+          view('characters.index', ['characters' => $data->json()["data"], 'pagination' => $pagination, 'params' => $request->validated()])->render(),
           3
         );
         Cache::put($key, $result, now()->addHours(5));
         return response($result, 200)->header('Content-Encoding', 'gzip');
       case 'html-none':
-        $result = view('characters.index', ['characters' => $data->json()["data"]])->render();
+        $result = view('characters.index', ['characters' => $data->json()["data"], 'pagination' => $pagination, 'params' => $request->validated()])->render();
         Cache::put($key, $result, now()->addHours(5));
         return response($result, 200);
     }
@@ -97,43 +127,9 @@ class CharacterController extends Controller
           return response($cache, 200);
       }
     }
-    $character = Character::find($character_id);
-    if (!$character) {
-      $data = Marvel::get('characters/' . $character_id);
-      if ($data->failed()) {
-        return $data->body();
-      }
-      $response = $data->json();
-      $character = Character::create([
-        'id' => $response["data"]["results"][0]["id"],
-        'name' => $response["data"]["results"][0]["name"],
-        'description' => $response["data"]["results"][0]["description"] ?? '',
-        'resourceURI' => $response["data"]["results"][0]["urls"][0]["url"],
-        'thumbnail' => $response["data"]["results"][0]["thumbnail"]["path"] . "/portrait_uncanny." . $response["data"]["results"][0]["thumbnail"]["extension"],
-        'etag' => $response["etag"],
-      ]);
-    }
-    if($character->comics()->count() < 2) {
-      $offset = 0;
-      $comics = [];
-      do {
-        $response = Marvel::get('characters/' . $character_id . '/comics', ['limit' => 100, 'offset' => $offset])->json();
-        $total = $response["data"]["total"];
-        foreach ($response["data"]["results"] as $comic) {
-          $comics[] = [
-            'id' => $comic["id"],
-            'title' => $comic["title"],
-            'issn' => $comic["issn"],
-            'description' => $comic["description"] ?? '',
-            'resourceURI' => $comic["urls"][0]["url"],
-            'thumbnail' => $comic["thumbnail"]["path"] . "/portrait_uncanny." . $comic["thumbnail"]["extension"],
-            'etag' => '',
-          ];
-        }
-        $offset += 100;
-      } while ($offset < $total);
-      Comic::upsert($comics, ['id'], ['title', 'issn', 'description', 'resourceURI', 'thumbnail']);
-      $character->comics()->sync(array_map(fn ($comic) => $comic['id'], $comics));
+    $character = $this->retriveCharacter($character_id);
+    if ($character == null) {
+      abort(404);
     }
     switch ($encoding) {
       case 'gzip':
@@ -153,11 +149,14 @@ class CharacterController extends Controller
   /**
    * Add Character as favorite to logged user .
    *
-   * @param  \App\Models\Character  $character
    * @return \Illuminate\Http\Response
    */
-  public function update(Character $character)
+  public function update($character_id)
   {
+    $character = $this->retriveCharacter($character_id);
+    if($character == null) {
+      return response()->json([], 404);
+    }
     Auth::user()->characters()->attach($character->id);
     return response()->json([], 204);
   }
@@ -165,11 +164,14 @@ class CharacterController extends Controller
   /**
    * Remove Cahracter as favorite to logged user.
    *
-   * @param  \App\Models\Character  $character
    * @return \Illuminate\Http\Response
    */
-  public function destroy(Character $character)
+  public function destroy($character_id)
   {
+    $character = $this->retriveCharacter($character_id);
+    if($character == null) {
+      return response()->json([], 404);
+    }
     Auth::user()->characters()->detach($character->id);
     return response()->json([], 204);
   }
@@ -177,5 +179,52 @@ class CharacterController extends Controller
   public function favorites()
   {
     return response()->json(Auth::user()->characters()->pluck('character_id'), 200);
+  }
+
+  private function retriveCharacter($character_id)
+  {
+    $character = Character::find($character_id);
+    if (!$character) {
+      $data = Marvel::get('characters/' . $character_id);
+      if ($data->failed() || gettype($data->json()) != "array" || $data->json() === []) {
+        return null;
+      }
+      $response = $data->json();
+      $character = Character::create([
+        'id' => $response["data"]["results"][0]["id"],
+        'name' => $response["data"]["results"][0]["name"],
+        'description' => $response["data"]["results"][0]["description"] ?? '',
+        'resourceURI' => $response["data"]["results"][0]["urls"][0]["url"],
+        'thumbnail' => $response["data"]["results"][0]["thumbnail"]["path"] . "/portrait_uncanny." . $response["data"]["results"][0]["thumbnail"]["extension"],
+        'etag' => $response["etag"],
+      ]);
+    }
+    if($character->comics()->count() < 2) {
+      $offset = 0;
+      $comics = [];
+      do {
+        $data = Marvel::get('characters/' . $character_id . '/comics', ['limit' => 100, 'offset' => $offset]);
+        if ($data->failed() || gettype($data->json()) != "array" || $data->json() === []) {
+          return null;
+        }
+        $response = $data->json();
+        $total = $response["data"]["total"];
+        foreach ($response["data"]["results"] as $comic) {
+          $comics[] = [
+            'id' => $comic["id"],
+            'title' => $comic["title"],
+            'issn' => $comic["issn"],
+            'description' => $comic["description"] ?? '',
+            'resourceURI' => $comic["urls"][0]["url"],
+            'thumbnail' => $comic["thumbnail"]["path"] . "/portrait_uncanny." . $comic["thumbnail"]["extension"],
+            'etag' => '',
+          ];
+        }
+        $offset += 100;
+      } while ($offset < $total);
+      Comic::upsert($comics, ['id'], ['title', 'issn', 'description', 'resourceURI', 'thumbnail']);
+      $character->comics()->sync(array_map(fn ($comic) => $comic['id'], $comics));
+    }
+    return $character;
   }
 }
